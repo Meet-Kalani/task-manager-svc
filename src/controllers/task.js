95ee5ea7df9ac46d29748e2fc5bd729f44b0aa80
Meetkalani <meetkalani2002@gmail.com>
2025-04-29 21:59:30 +0530

    feat: setup routes for task

---
+++ src/controllers/task.js
@@ -0,0 +1,17 @@
+const getAllTasks = (req, res, next) => {};
+
+const getTaskById = (req, res, next) => {};
+
+const createTask = (req, res, next) => {};
+
+const updateTask = (req, res, next) => {};
+
+const deleteTask = (req, res, next) => {};
+
+module.exports = {
+  getAllTasks,
+  getTaskById,
+  createTask,
+  updateTask,
+  deleteTask
+};

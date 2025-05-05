7c8b9e5038135705c8305a9166319c579ba7519b
Meetkalani <meetkalani2002@gmail.com>
2025-05-05 23:03:18 +0530

    feat: update app.js to include user, priority, status, and tag routes

--- app.js
+++ app.js
@@ -3,13 +3,22 @@ const express = require('express');
 
 const bodyParser = require('body-parser');
 const taskRoutes = require('./src/routes/task');
+const userRoutes = require('./src/routes/user');
+const priorityRoutes = require('./src/routes/priority');
+const statusRoutes = require('./src/routes/status');
+const tagRoutes = require('./src/routes/tag');
 const db = require('./src/utils/database');
 const errorHandler = require('./src/middlewares/errorHandler');
 const logger = require('./src/utils/logger');
 
 const app = express();
 app.use(bodyParser.json());
-app.use('/tasks', taskRoutes);
+
+app.use('/task', taskRoutes);
+app.use('/user', userRoutes);
+app.use('/priority', priorityRoutes);
+app.use('/status', statusRoutes);
+app.use('/tag', tagRoutes);
 
 app.use((req, res, next) => {
   res.status(404).json({ message: 'API endpoint not found', success: false });

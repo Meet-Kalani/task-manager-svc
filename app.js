ca441ce253afd91e6760adec064a51f37b760efc
Meet Kalani <meet.kalani@zuru.com>
2025-05-29 12:07:04 +0530

    feat: add CORS support

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 const express = require('express');
 
 const bodyParser = require('body-parser');
+const cors = require('cors');
 const taskRoutes = require('./src/components/task/route');
 const userRoutes = require('./src/components/user/route');
 const priorityRoutes = require('./src/components/priority/route');
@@ -13,6 +14,7 @@ const logger = require('./src/utils/logger');
 
 const app = express();
 app.use(bodyParser.json());
+app.use(cors());
 
 app.use('/task', taskRoutes);
 app.use('/user', userRoutes);
